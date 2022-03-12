1f587c4d827978818da39af60c2d8efeb3aca605
Adam Sadler <adambsadler@gmail.com>
2022-03-12 13:12:06 -0500

    Initial commit

--- PtG Roster/ContentView.swift
+++ PtG Roster/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("PtG Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .aspectRatio(contentMode: .fill)
+            VStack {
+                Image("PtG Logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 350.0)
+                Divider()
+                Button("My Armies") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
