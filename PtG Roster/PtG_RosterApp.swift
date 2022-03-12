4ff6b88786835d99ee245a8e47b1ade60f27b634
Adam Sadler <adambsadler@gmail.com>
2022-03-12 11:25:17 -0500

    Initial Commit

---
+++ PtG Roster/PtG_RosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PtG_RosterApp.swift
+//  PtG Roster
+//
+//  Created by Adam Sadler on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PtG_RosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
